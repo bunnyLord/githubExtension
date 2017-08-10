cb7521b576356c6dd634b2ed5a2f7f31a5118e81
TA035447 <Tatiana.Alexenko@Cerner.com>
2017-08-10 11:03:28 -0500

    Add git REST call. switching to Cerner github now :(

--- extension/inject.js
+++ extension/inject.js
@@ -1,12 +1,20 @@
 (function() {
 
-	// just place a div at top right
-	var div = document.createElement('div');
-	div.style.position = 'fixed';
-	div.style.top = 0;
-	div.style.right = 0;
-	div.textContent = 'Injected!';
-	document.body.appendChild(div);
+	// // just place a div at top right
+	// var div = document.createElement('div');
+	// div.style.position = 'fixed';
+	// div.style.top = 0;
+	// div.style.right = 0;
+	// div.textContent = 'Injected!';
+	// document.body.appendChild(div);
+	var url = "https://api.github.com/users/bunnyLord/repos";
+	fetch(url) // Call the fetch function passing the url of the API as a parameter
+.then(function() {
+    // Your code for handling the data you get from the API
+})
+.catch(function() {
+    // This is where you run code if the server returns any errors
+});
 
 	alert('inserted self... giggity');
 
