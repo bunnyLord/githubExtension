ce97348909d0eb43f47672d45bf1c02c7902d274
TA035447 <Tatiana.Alexenko@Cerner.com>
2017-08-10 10:52:36 -0500

    Add inject.js and call it from background.js

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   const url = tab.url;
 	
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="green"'
-  });
+chrome.tabs.executeScript(tab.ib, {
+		file: 'inject.js'
+	});
 });
